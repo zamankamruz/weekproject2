66b87e12f0424db1afadb8c1a188a7d4c3b598bd
zamankamruz <zamankamruz465@gmail.com>
2024-09-26 10:54:01 +0600

    second project

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name = "Donald Trump";
+        $age = "75";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        $cookie = cookie($cookieName, $cookieValue, $minutes, $path, $domain, $secure, $httpOnly);
+
+        return response()->json($data, 200)->cookie($cookie);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::get('/profile/{id}', [ProfileController::class, 'index']);
+
+
+
+
